9c0c878a8e1c0e33db1413cd69144326bda8f99f
JAYASRIRAMAN-V-S <jayasriraman2003@gmail.com>
2023-09-04 22:09:25 +0530

    new one

--- Hello.py
+++ Hello.py
@@ -4,6 +4,6 @@ x = input("Enter the keyword: ")
 
 if x == "hello":
     print("the key is correct")
-    print("The key is reverted and this is the correct directoryS")
+    print("The key is reverted and this is the correct directory")
 else:
     print("key is incorrect")
